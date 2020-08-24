676aa6b7d760e2102f81faab02e3e1729f3cde78
DanielFSeg <69900272+DanielFSeg@users.noreply.github.com>
2020-08-24 13:19:20 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions that cache the inverse of a matrix
+## Pass the result of a makeCacheMatrix call to cacheSolve 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # Allows to create a new object that
+                                            # stores a matrix and its inverse
+    
+    inv <- NULL # Create the inverse matrix variable placeholder
+    
+    set <- function(y) { # Set a new matrix in the cache and clear the inverse
+        x <<- y          # cache for a new matrix
+        inv <<- NULL     #
+    }
+    
+    get <- function() x  # Get the original matrix
+    
+    setinverse <- function(inverse) inv <<- inverse # Set the invermatrix, not
+    # the calculations
+    
+    getinverse <- function() inv   # Get the stored inverse matrix
+    
+    list(set = set, get = get,     # Pool list for the functions created
+         setinverse = setinverse,  # which allows the use of $
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#' Compute and cache the inverse of a matrix
+#' using the function created above
+#' 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinverse() # Store the inverse matrix in the cache
+    
+    if(!is.null(inv)) {                           # Check if the inverse matrix exists
+        message("getting cached matrix inverse")
+        return(inv)
+    }
+    
+    data <- x$get() # Get the original matrix
+    
+    inv <- solve(data, ...) # Calculate the inverse matrix
+    
+    x$setinverse(inv) # Set the inverse matrix in the cache
+    
+    inv # return the inverse matrix
 }
+
+# Examples ----
+
+dfs <- matrix(rnorm(25), 5, 5) # Creating a random 5x5 matrix
+
+dfs_cache <- makeCacheMatrix(dfs) # Store the matrix with the function
+                                  # makeCacheMatrix() in the new object
+
+dfs_cache$get() == dfs # Checking if cache and matrix are the same
+
+cacheSolve(dfs_cache) # Calculate and store the inverse matrix in the object
+
+dfs_cache$getinverse() == solve(dfs) # Checking if cache and inverse matrix are the same
+
